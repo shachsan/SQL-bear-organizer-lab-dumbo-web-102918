fad4524200b83deacd54d4b2cfab5422c3528d29
SANJAY K SAI <shachsan@hotmail.com>
2018-11-04 23:07:40 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,10 +7,10 @@ values
   "Rowdy", 23, "male", "dark brown", "angry", 1
 ),
 (
-  "Lisa", 33, "female", "light brown", "normal", 0
+  "Tabitha", 33, "female", "light brown", "normal", 0
 ),
 (
-  "Jugar", 15, "male", "brown", "cool", 1
+  "Sergeant Brown", 15, "male", "brown", "cool", 1
 ),
 (
   "Bobby", 13, "male", "brown", "cool", 1
