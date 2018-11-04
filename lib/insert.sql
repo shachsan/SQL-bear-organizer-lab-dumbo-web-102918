5ca877e8b9660946f8bb38e30066e52e838bd2dc
SANJAY K SAI <shachsan@hotmail.com>
2018-11-04 23:00:06 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,8 @@
-INSERT INTO bears ()
+INSERT INTO bears (name TEXT,age INTEGER,
+gender TEXT,
+color TEXT,
+temperament TEXT,
+alive INTEGER)
 values (
   "Bobby", 13, "male", "brown", "cool", 1
 );
