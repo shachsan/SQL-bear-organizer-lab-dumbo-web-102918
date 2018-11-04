b6cb8571dd131d476d18e9b8355af06b4ca16858
SANJAY K SAI <shachsan@hotmail.com>
2018-11-04 23:04:50 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,10 +4,10 @@ values
   "Bobby", 13, "male", "brown", "cool", 1
 ),
 (
-  "Tiger", 23, "male", "dark brown", "ang", 1
+  "Tiger", 23, "male", "dark brown", "angry", 1
 ),
 (
-  "Bobby", 13, "male", "brown", "cool", 1
+  "Lisa", 13, "male", "brown", "cool", 1
 ),
 (
   "Bobby", 13, "male", "brown", "cool", 1
