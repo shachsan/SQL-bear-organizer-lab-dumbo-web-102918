ad888af64664a11a3ca924ce028f68aec53ac8ee
SANJAY K SAI <shachsan@hotmail.com>
2018-11-04 23:14:04 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,7 +19,7 @@ values
   "Grinch", 9, "male", "white", "cool", 1
 ),
 (
-  "Wendy", 27, "male", "brown", "rude", 0
+  "Wendy", 27, "female", "brown", "rude", 0
 ),
 (
   null, 7, "male", "brown", "nice", 1
