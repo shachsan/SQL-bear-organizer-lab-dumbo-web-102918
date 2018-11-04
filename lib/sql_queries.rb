b693e79500aadaf6654b753461a00be86703e73c
SANJAY K SAI <shachsan@hotmail.com>
2018-11-04 23:36:53 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT "
+  "SELECT name, age "
 end
 
 def selects_most_prominent_color_and_returns_with_count
